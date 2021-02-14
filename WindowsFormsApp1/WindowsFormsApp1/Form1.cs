5c03621eb8a79a9fff9d6c1f9a6d6b9cf79ce6ad
Dan1cos <Dan1cos@users.noreply.github.com>
2021-02-14 18:53:55 +0200

    Opening file func

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Text files|*.txt|All Files|*.*";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(dlg.FileName);
+            }
+        }
+    }
+}
